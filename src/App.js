b00f89385eded843229a4dd23f949827312f940d
“Kawsahr <kawshar798@gmail.com>
2020-06-23 10:13:24 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 
 useEffect(() => {
 localStorage.setItem("expenses",JSON.stringify(expenses));
-});
+},[expenses]);
 
   const handleCharge = (e) =>{
     setCharge(e.target.value);
